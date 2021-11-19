6a3f2fb2427fd58b6dce2d4f32ce47cf724f14d0
uzair-ali10 <the.uzairali10@gmail.com>
2021-11-19 19:12:08 +0530

    initial

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func RootRedirect(c *gin.Context) {
+	c.Redirect(302, "https://uzair.biz/")
+}
+
+func ResumeRedirect(c *gin.Context) {
+	c.Redirect(302, "https://drive.google.com/file/d/1PW6qGyz9nnC2FGYwoPXDRIn3a4gcYMMg/view?usp=sharing")
+}
+
+func GithubRedirect(c *gin.Context) {
+	c.Redirect(302, "https://github.com/uzair-ali10")
+}
+
+func LinkedinRedirect(c *gin.Context) {
+	c.Redirect(302, "https://www.linkedin.com/in/uzair-ali10")
+}
+
+func TwitterRedirct(c *gin.Context) {
+	c.Redirect(302, "https://twitter.com/UzairAli101")
+}
+
+func DevRedirect(c *gin.Context) {
+	c.Redirect(302, "https://dev.to/uzairali10/")
+}
+
+func LeetcodeRedirect(c *gin.Context) {
+	c.Redirect(302, "https://leetcode.com/uzair-ali10/")
+}
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/", RootRedirect)
+	server.GET("/resume", ResumeRedirect)
+	server.GET("/github", GithubRedirect)
+	server.GET("/lkdn", LinkedinRedirect)
+	server.GET("/linkedin", LinkedinRedirect)
+	server.GET("/twitter", TwitterRedirct)
+	server.GET("/dev", DevRedirect)
+	server.GET("/leetcode", LeetcodeRedirect)
+
+
+	server.Run(":8080")
+}

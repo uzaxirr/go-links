19ef455451a330f066b304a562c6e0d3779a2ca9
uzair-ali10 <the.uzairali10@gmail.com>
2021-12-31 00:11:50 +0530

    New User Name

--- main.go
+++ main.go
@@ -9,27 +9,27 @@ func RootRedirect(c *gin.Context) {
 }
 
 func ResumeRedirect(c *gin.Context) {
-	c.Redirect(302, "https://drive.google.com/file/d/1zYg0T3f8lLLpgewshXUvxU1Ei-KbqhWZ/view?usp=sharing")
+	c.Redirect(302, "https://drive.google.com/file/d/1sasOj2nxrrHZ1xad0EztJ8SzZQRGiYSP/view?usp=sharing")
 }
 
 func GithubRedirect(c *gin.Context) {
-	c.Redirect(302, "https://github.com/uzair-ali10")
+	c.Redirect(302, "https://github.com/uzaxirr/")
 }
 
 func LinkedinRedirect(c *gin.Context) {
-	c.Redirect(302, "https://www.linkedin.com/in/uzair-ali10")
+	c.Redirect(302, "https://www.linkedin.com/in/uzaxirr/")
 }
 
 func TwitterRedirct(c *gin.Context) {
-	c.Redirect(302, "https://twitter.com/UzairAli101")
+	c.Redirect(302, "https://twitter.com/uzaxirr/")
 }
 
 func DevRedirect(c *gin.Context) {
-	c.Redirect(302, "https://dev.to/uzairali10/")
+	c.Redirect(302, "https://dev.to/uzaxirr/")
 }
 
 func LeetcodeRedirect(c *gin.Context) {
-	c.Redirect(302, "https://leetcode.com/uzair-ali10/")
+	c.Redirect(302, "https://leetcode.com/uzairmq/")
 }
 
 func main() {
@@ -39,6 +39,8 @@ func main() {
 	server.GET("/resume", ResumeRedirect)
 	server.GET("/cv", ResumeRedirect)
 	server.GET("/github", GithubRedirect)
+	server.GET("/Github", GithubRedirect)
+	server.GET("/GitHub", GithubRedirect)
 	server.GET("/lkdn", LinkedinRedirect)
 	server.GET("/linkedin", LinkedinRedirect)
 	server.GET("/twitter", TwitterRedirct)

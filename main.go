445537ae8bbea81954e1fb99563b58643e6b0ec2
uzair-ali10 <the.uzairali10@gmail.com>
2021-12-31 00:17:52 +0530

    dev.to add

--- main.go
+++ main.go
@@ -45,6 +45,7 @@ func main() {
 	server.GET("/linkedin", LinkedinRedirect)
 	server.GET("/twitter", TwitterRedirct)
 	server.GET("/dev", DevRedirect)
+	server.GET("/dev.to", DevRedirect)
 	server.GET("/leetcode", LeetcodeRedirect)
 
 	server.Run()
